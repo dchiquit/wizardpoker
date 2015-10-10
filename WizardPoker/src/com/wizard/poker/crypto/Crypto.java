c727f2f5ae957c3a662eca58e0021ad3e5094069
Daniel Chiquito <daniel.chiquito@gmail.com>
2015-10-10 15:10:53 -0400

    stuff I forgot

---
+++ WizardPoker/src/com/wizard/poker/crypto/Crypto.java
@@ -0,0 +1,15 @@
+package com.wizard.poker.crypto;
+
+import java.math.BigInteger;
+
+import com.wizard.poker.profile.PrivateProfile;
+import com.wizard.poker.profile.PublicProfile;
+
+public class Crypto {
+	public static BigInteger encrypt(PublicProfile publicProfile, byte[] plaintext) {
+		return null;
+	}
+	public static BigInteger decrypt(PrivateProfile privateProfile, BigInteger ciphertext) {
+		return null;
+	}
+}
---
+++ WizardPoker/src/com/wizard/poker/crypto/rsa/RSACrypto.java
@@ -0,0 +1,18 @@
+package com.wizard.poker.crypto.rsa;
+
+import java.math.BigInteger;
+
+public class RSACrypto {
+
+	public static BigInteger encrypt(RSAPublicProfile publicProfile,
+			BigInteger plaintext) {
+		return plaintext.modPow(publicProfile.getExponent(),
+				publicProfile.getModulus());
+	}
+
+	public static BigInteger decrypt(RSAPrivateProfile privateProfile,
+			BigInteger ciphertext) {
+		return ciphertext.modPow(privateProfile.getPrivateExponent(),
+				privateProfile.getModulus());
+	}
+}
