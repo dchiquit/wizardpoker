c05b2e2998ec17ec2668b168046963b2e355cba0
Daniel Chiquito <daniel.chiquito@gmail.com>
2015-10-10 17:23:19 -0400

    did shit

--- WizardPoker/src/com/wizard/poker/crypto/rsa/RSAPrivateProfile.java
+++ WizardPoker/src/com/wizard/poker/crypto/rsa/RSAPrivateProfile.java
@@ -1,53 +1,57 @@
 package com.wizard.poker.crypto.rsa;
 
 import java.math.BigInteger;
-import java.security.Key;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPublicKeySpec;
+import java.util.Random;
 
 import com.wizard.poker.profile.PrivateProfile;
 
 public class RSAPrivateProfile implements PrivateProfile {
 
-	private final BigInteger publicExponent;
 	private final BigInteger privateExponent;
+	private final BigInteger publicExponent;
 	private final BigInteger modulus;
 
 	public RSAPrivateProfile(BigInteger publicExponent,
-			BigInteger privateExponent, BigInteger modulus) {
+			BigInteger privateExponent, BigInteger publicModulus,
+			BigInteger modulus) {
+		this.privateExponent = publicExponent;
 		this.publicExponent = publicExponent;
-		this.privateExponent = privateExponent;
-		this.modulus = modulus;
+		this.modulus = publicModulus;
 	}
 
 	public RSAPrivateProfile() throws NoSuchAlgorithmException,
 			InvalidKeySpecException {
-		KeyPairGenerator kpg;
-		kpg = KeyPairGenerator.getInstance("RSA");
-		kpg.initialize(2048);
-		KeyPair kp = kpg.genKeyPair();
-		KeyFactory fact = KeyFactory.getInstance("RSA");
-		RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(),
-				RSAPublicKeySpec.class);
-		RSAPublicKeySpec priv = fact.getKeySpec(kp.getPrivate(),
-				RSAPublicKeySpec.class);
-		this.publicExponent = pub.getPublicExponent();
-		this.privateExponent = priv.getPublicExponent();
-		this.modulus = pub.getModulus();
-		System.out.println(pub.getPublicExponent() + "," + pub.getModulus());
-		System.out.println(pub.getPublicExponent() + "," + pub.getModulus());
+//		 KeyPairGenerator kpg;
+//		 kpg = KeyPairGenerator.getInstance("RSA");
+//		 kpg.initialize(2048);
+//		 KeyPair kp = kpg.genKeyPair();
+//		 KeyFactory fact = KeyFactory.getInstance("RSA");
+//		 RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(),
+//		 RSAPublicKeySpec.class);
+//		 RSAPrivateKeySpec priv = fact.getKeySpec(kp.getPrivate(),
+//		 RSAPrivateKeySpec.class);
+//		
+//		 this.privateExponent = priv.getPrivateExponent();
+//		 this.publicExponent = pub.getPublicExponent();
+//		 this.modulus = pub.getModulus();
+//		 System.out.println(publicExponent);
+//		 // d*2 = 1 (mod 8) = 1,9,
+		BigInteger p = BigInteger.probablePrime(160, new Random());
+		BigInteger q = BigInteger.probablePrime(160, new Random());
+		this.modulus = p.multiply(q);
+		BigInteger tot = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+		this.publicExponent = new BigInteger("65537");
+		
 	}
 
 	public BigInteger getPublicExponent() {
-		return publicExponent;
+		return privateExponent;
 	}
 
 	public BigInteger getPrivateExponent() {
-		return privateExponent;
+		return modulus;
 	}
 
 	public BigInteger getModulus() {
