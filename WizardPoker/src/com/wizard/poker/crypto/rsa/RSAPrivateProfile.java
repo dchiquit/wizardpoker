ab78d57fac3cfa57f17764b50edf0121a61e0283
rainbowPhoenix <ndtobey3@gmail.com>
2015-10-10 21:25:09 -0400

    RSA working?

--- WizardPoker/src/com/wizard/poker/crypto/rsa/RSAPrivateProfile.java
+++ WizardPoker/src/com/wizard/poker/crypto/rsa/RSAPrivateProfile.java
@@ -7,6 +7,7 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 
 import com.wizard.poker.profile.PrivateProfile;
@@ -23,6 +24,18 @@ public class RSAPrivateProfile implements PrivateProfile {
 		this.privateExponent = privateExponent;
 		this.modulus = modulus;
 	}
+	
+	public static void main(String[] args) {
+		try {
+			new RSAPrivateProfile();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public RSAPrivateProfile() throws NoSuchAlgorithmException,
 			InvalidKeySpecException {
@@ -33,13 +46,30 @@ public class RSAPrivateProfile implements PrivateProfile {
 		KeyFactory fact = KeyFactory.getInstance("RSA");
 		RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(),
 				RSAPublicKeySpec.class);
-		RSAPublicKeySpec priv = fact.getKeySpec(kp.getPrivate(),
-				RSAPublicKeySpec.class);
+		RSAPrivateKeySpec priv = fact.getKeySpec(kp.getPrivate(),
+				RSAPrivateKeySpec.class);
 		this.publicExponent = pub.getPublicExponent();
-		this.privateExponent = priv.getPublicExponent();
+		this.privateExponent = priv.getPrivateExponent();
 		this.modulus = pub.getModulus();
-		System.out.println(pub.getPublicExponent() + "," + pub.getModulus());
-		System.out.println(pub.getPublicExponent() + "," + pub.getModulus());
+		System.out.printf("Modulus: %d\nModulus: %d\n", pub.getModulus(), priv.getModulus());
+		System.out.printf("Public Key: %d\n", pub.getPublicExponent());
+		System.out.printf("Private Key: %d\n", priv.getPrivateExponent());
+		//System.out.printf("ed mod n = %d\n", this.publicExponent.multiply(privateExponent).mod(modulus));
+		for(int i = 0; i < 10000; i++) {
+			/*if(!BigInteger.valueOf(i).modPow(privateExponent, modulus).modPow(publicExponent, modulus).equals(BigInteger.valueOf(i))) {
+				System.out.printf("FAIL at %d\n", i);
+			}
+			if(!BigInteger.valueOf(i).modPow(publicExponent, modulus).modPow(privateExponent, modulus).equals(BigInteger.valueOf(i))) {
+				Syste.out.printf("FAIL at %d\n", i);
+			}*/
+			BigInteger bi = BigInteger.valueOf(i);
+			if(!encrypt(decrypt(bi)).equals(bi)) {
+				System.out.printf("FAIL at %d\n", i);
+			}
+			if(!decrypt(encrypt(bi)).equals(bi)) {
+				System.out.printf("FAIL at %d\n", i);
+			}
+		}
 	}
 
 	public BigInteger getPublicExponent() {
