63e13567c53ac1bac3d4e069c56041d76e5154bf
rainbowPhoenix <ndtobey3@gmail.com>
2015-10-10 21:36:07 -0400

    RSA

--- WizardPoker/src/test/Test.java
+++ WizardPoker/src/test/Test.java
@@ -1,8 +1,10 @@
 package test;
 
+import com.wizard.poker.crypto.rsa.RSAPrivateProfile;
+
 
 public class Test {
 	public static void main(String[] args) {
-		
+		RSAPrivateProfile a = new RSAPrivateProfile();
 	}
 }
